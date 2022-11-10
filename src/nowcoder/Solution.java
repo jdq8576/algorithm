cca3b4a3a3716710e92e2bf1c6773b8238ad2c90
蒋东旗 <jdq8576@github.com>
2022-11-10 18:23:33 +0800

    some changes

--- src/nowcoder/Solution.java
+++ src/nowcoder/Solution.java
@@ -1367,7 +1367,7 @@ public class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         for (int i = 0; i < n; i++) {
-            for (int j = m - 2; j >= 0; j++) {
+            for (int j = m - 2; j >= 0; j--) {
                 matrix[i][j] = matrix[i][j] == 1 ? matrix[i][j] + matrix[i][j + 1] : 0;
             }
         }
